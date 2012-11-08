17ab92f9555357593d15aaacce2259ec2e99a0ec
Charlie Hothersall <chothersall@gmail.com>
2012-11-08 22:29:46 +0000

    Tidied up source.

--- Milestone 2/MAlice.cpp
+++ Milestone 2/MAlice.cpp
@@ -23,30 +23,27 @@ void printTree(pANTLR3_BASE_TREE ast) {
 	print(ast, 0);
 }
 
-int main(int argc, char* argv[])
-{
-  pANTLR3_INPUT_STREAM input;
-  pMAliceLexer lex;
-  pANTLR3_COMMON_TOKEN_STREAM tokens;
-  pMAliceParser parser;
+int main(int argc, char* argv[]) {
+	pANTLR3_INPUT_STREAM input;
+	pMAliceLexer lex;
+	pANTLR3_COMMON_TOKEN_STREAM tokens;
+	pMAliceParser parser;
 
-  assert(argc > 1);
-  input = antlr3AsciiFileStreamNew((pANTLR3_UINT8)argv[1]);
-  lex = MAliceLexerNew(input);
-  tokens = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT,
-                                            TOKENSOURCE(lex));
-  parser = MAliceParserNew(tokens);
+	assert(argc > 1);
+	input = antlr3AsciiFileStreamNew((pANTLR3_UINT8) argv[1]);
+	lex = MAliceLexerNew(input);
+	tokens = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lex));
+	parser = MAliceParserNew(tokens);
 
-  MAliceParser_program_return r = parser->program(parser);
+	MAliceParser_program_return r = parser->program(parser);
+	pANTLR3_BASE_TREE tree = r.tree;
 
-  pANTLR3_BASE_TREE tree = r.tree;
+	printTree(tree);
 
-  printTree(tree);
+	parser->free(parser);
+	tokens->free(tokens);
+	lex->free(lex);
+	input->close(input);
 
-  parser->free(parser);
-  tokens->free(tokens);
-  lex->free(lex);
-  input->close(input);
-
-  return 0;
+	return 0;
 }
