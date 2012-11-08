c083061c6ec74599e9d2ed8ef19f9aedf6c754c6
Charlie Hothersall <chothersall@gmail.com>
2012-11-08 23:15:59 +0000

    With Owen: wrote parseFile method. Main method now takes multiple filename arguments, so *.alice works.

--- Milestone 2/MAlice.cpp
+++ Milestone 2/MAlice.cpp
@@ -1,7 +1,5 @@
 #include "MAliceLexer.h"
 #include "MAliceParser.h"
-#include <cassert>
-#include <string>
 #include <iostream>
 
 using namespace std;
@@ -23,27 +21,34 @@ void printTree(pANTLR3_BASE_TREE ast) {
 	print(ast, 0);
 }
 
-int main(int argc, char* argv[]) {
+void parseFile(pANTLR3_UINT8 filename, bool doPrintTree) {
 	pANTLR3_INPUT_STREAM input;
 	pMAliceLexer lex;
 	pANTLR3_COMMON_TOKEN_STREAM tokens;
 	pMAliceParser parser;
 
-	assert(argc > 1);
-	input = antlr3AsciiFileStreamNew((pANTLR3_UINT8) argv[1]);
+	input = antlr3AsciiFileStreamNew(filename);
 	lex = MAliceLexerNew(input);
 	tokens = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lex));
 	parser = MAliceParserNew(tokens);
 
 	MAliceParser_program_return r = parser->program(parser);
-	pANTLR3_BASE_TREE tree = r.tree;
-
-	printTree(tree);
+	if (doPrintTree) {
+		pANTLR3_BASE_TREE tree = r.tree;
+		printTree(tree);
+	}
 
 	parser->free(parser);
 	tokens->free(tokens);
 	lex->free(lex);
 	input->close(input);
+}
+
+int main(int argc, char* argv[]) {
+	for (int i = 1; i < argc; i++) {
+		parseFile((pANTLR3_UINT8) argv[i], true);
+		cout << endl;
+	}
 
 	return 0;
 }
