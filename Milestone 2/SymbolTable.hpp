7b02a062d703934e92e5cd8116e159bd81eb5293
Charlie Hothersall <chothersall@gmail.com>
2012-11-09 20:26:51 +0000

    Included Identifier header, and code style tweak.

--- Milestone 2/SymbolTable.hpp
+++ Milestone 2/SymbolTable.hpp
@@ -11,11 +11,11 @@ class SymbolTable {
 	SymbolTable* _encSymTable;
 	boost::unordered_map<string, Identifier*> _dict;
 
-	public:
-		SymbolTable(SymbolTable* st);
-		void add(string name, Identifier* obj);
-		Identifier* lookupCurrLevelOnly(string name);
-		Identifier* lookupCurrLevelAndEnclosingLevels(string name);
+public:
+	SymbolTable(SymbolTable* st);
+	void add(string name, Identifier* obj);
+	Identifier* lookupCurrLevelOnly(string name);
+	Identifier* lookupCurrLevelAndEnclosingLevels(string name);
 };
 
 #endif
