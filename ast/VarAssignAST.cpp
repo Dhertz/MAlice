7a44905fb548d5835273fb9e0a55c508b8f67022
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-11-25 20:04:31 +0000

    Safety fixed on VarAssign.

--- ast/VarAssignAST.cpp
+++ ast/VarAssignAST.cpp
@@ -13,23 +13,21 @@ void VarAssignAST::check() {
  	boost::shared_ptr<Identifier> var = _st->lookupCurrLevelAndEnclosingLevels(_varName);
 
 	if (!var) {
-		cerr << "Line " << _lineNo << " - " << "Cannot assign to non-exisitant variable " << _varName  << "." << endl;
+		cerr << "Line " << _lineNo << " - " << "Cannot assign to non-exisitent variable " << _varName  << "." << endl;
 	} else if (var->getBaseName() != "Variable") {
 		cerr << "Line " << _lineNo << " - " << "Cannot declare to " << _varName << " as it is a " 
 			<< var->getBaseName() << ", which is not an r-value" << endl;
 	} else {
 		boost::shared_ptr<Variable> varCasted = boost::shared_polymorphic_downcast<Variable>(var);
 
-		// TODO: patch this up once all ExprAST::check() cases make the expression point to a type (i.e. not null)
-		if(!_expr) {
+		if(!_expr || !_expr->getTypeName()) {
 			cerr << "Line " << _lineNo << " - " << "Cannot assign variable" << _varName << "to bad expression." << endl;
-		} else if (_expr->getTypeName()) {
-			// _expr points to a type, so we can check it properly
-			if (_expr->getTypeName()->getTypeName() != varCasted->getTypeName()->getTypeName()) {
+		} else if (!varCasted->getTypeName()) {
+			cerr << "Line " << _lineNo << " - " << "Variable " << _varName << " has bad type" << endl;
+		} else if (_expr->getTypeName()->getTypeName() != varCasted->getTypeName()->getTypeName()) {
 				cerr << "Line " << _lineNo << " - " << "lhs (" << varCasted->getTypeName()->getTypeName() << ") and rhs (" << _expr->getTypeName()->getTypeName() << ") not type compatible" << endl;		
-			} else {
+		} else {
 				_varObj = varCasted;
-			}
 		}
 	}	
 }
