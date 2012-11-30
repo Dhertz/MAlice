c562056e2d81f9bc674a9cf20aa3f1ce265f7945
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-11-30 21:09:45 +0000

    Fixed bug where AST::printNode() was seg faulting in some cases.

--- ast/AST.cpp
+++ ast/AST.cpp
@@ -13,14 +13,22 @@ void AST::printNode(boost::shared_ptr<ASTNode> node, int level) {
         cout << "--";
     }
 
-	node->print();
-	vector< boost::shared_ptr<ASTNode> >::iterator i;
-	for (i = node->getChildren().begin(); i != node->getChildren().end(); ++i) {
-		if (!(*i)) {
-			cout << "node is null" << endl;
-		} else {
-			printNode(*i, ++level);
+	if (node) {
+		node->print();
+		
+		vector< boost::shared_ptr<ASTNode> > children = node->getChildren();
+		vector< boost::shared_ptr<ASTNode> >::iterator i;
+		for (i = children.begin(); i != children.end(); ++i) {
+			boost::shared_ptr<ASTNode> childNode = *i;
+			if (!childNode) {
+				cout << "node is null" << endl;
+			} else {
+				printNode(childNode, ++level);
+			}
 		}
+		cout << endl;
+	} else {
+		cout << "node is null" << endl;
 	}
-	cout << endl;
+
 }	
